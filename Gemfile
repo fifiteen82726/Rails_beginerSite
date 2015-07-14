b9202368561b00756eabdb229b30c027eb9c2735
fifiteen82726 <fifiteen82726@gmail.com>
2015-07-15 00:58:00 +0800

    新增devise驗證使用者身份,移除root的welcome

--- Gemfile
+++ Gemfile
@@ -38,3 +38,9 @@ gem 'spring',        group: :development
 
 # Use debugger
 # gem 'debugger', group: [:development, :test]
+
+
+# 自己新增的套件
+# 使用者認證
+gem 'devise'
+gem 'bcrypt'
--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,9 @@ module RailsBeginersite
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    #使用者驗證,若是需要部署到heroku上
+    config.assets.initialize_on_precompile = false
+    
   end
 end
